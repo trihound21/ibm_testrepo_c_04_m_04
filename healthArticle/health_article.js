f12c3b849f2ba98c634000a887b389847207a520
trihound21 <toddmoran@hotmail.com>
2025-07-15 17:28:33 -0400

    Updated and tested health_article lab.

---
+++ healthArticle/health_article.js
@@ -0,0 +1,62 @@
+// Variable - XMLHttpRequest
+var xhr = new XMLHttpRequest();
+
+// Variable - URL for JSON file
+var url = './health_article.json';
+
+// GET request
+xhr.open('GET', url, true);
+
+// JSON Response
+xhr.responseType = 'json';
+
+// FUNCTIONS
+xhr.onload = function() {
+    var articles = xhr.response.articles;
+    var articlesDiv = document.getElementById('articles');
+
+// Get articles
+articles.forEach(function(article) {
+    var articleDiv = document.createElement('div');
+    articleDiv.classList.add('article');
+
+    var title = document.createElement('h2');
+    title.textContent = article.title;
+
+    var description = document.createElement('p');
+    description.textContent = article.description;
+
+    var waysHeader = document.createElement('h3');
+    waysHeader.textContent = 'Ways to Achieve:';
+
+    var waysList = document.createElement('ul');
+    article.ways_to_achieve.forEach(function(way) {
+      var listItem = document.createElement('li');
+      listItem.textContent = way;
+      waysList.appendChild(listItem);
+    });
+
+    var benefitsHeader = document.createElement('h3');
+    benefitsHeader.textContent = 'Benefits:';
+
+    var benefitsList = document.createElement('ul');
+    article.benefits.forEach(function(benefit) {
+      var listItem = document.createElement('li');
+      listItem.textContent = benefit;
+      benefitsList.appendChild(listItem);
+    });
+
+    articleDiv.appendChild(title);
+    articleDiv.appendChild(description);
+    articleDiv.appendChild(waysHeader);
+    articleDiv.appendChild(waysList);
+    articleDiv.appendChild(benefitsHeader);
+    articleDiv.appendChild(benefitsList);
+
+    articlesDiv.appendChild(articleDiv);
+  });
+}
+
+// XHR Send
+xhr.send();
+
